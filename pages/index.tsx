2288715cf34d19b86e2891b10ef9d5e004df45ca
Sai Han <thesaihan@gmail.com>
2022-01-27 20:47:19 +0630

    Configure bootstrap icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { PageLayout } from "../components";
 import styles from "../styles/Home.module.scss";
 
@@ -10,7 +8,9 @@ const Home: NextPage = () => {
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>Welcome to E360</h1>
-          <p className={styles.description}>Get started with E360 Case Management System.</p>
+          <p className={styles.description}>
+            <i className="bi bi-triangle-half"></i> Get started with E360 Case Management System.
+          </p>
         </main>
       </div>
     </PageLayout>
