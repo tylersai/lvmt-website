44a22431cb9b01597376d8bc8cfcb91b340d0590
Sai Han <thesaihan@gmail.com>
2022-02-02 12:03:00 +0630

    Button component accepts both "button" and "a" as dynamic underlying prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,28 @@
 import classNames from "classnames";
-import React, { FC, ReactHTML } from "react";
+import React, { FC } from "react";
 import styles from "../styles/Button.module.scss";
 
 type ButtonColor = "primary" | "default" | "secondary";
+type ButtonComponent = "button" | "a";
 
 interface ButtonProps {
-  component?: ReactHTML;
+  component?: ButtonComponent;
   color?: ButtonColor;
   className?: string;
+  href?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const Button: FC<ButtonProps> = ({ component, color = "primary", className, onClick, children }) =>
-  React.createElement("button", { className: classNames(styles[`Button_${color}`], className), onClick }, children);
+export const Button: FC<ButtonProps> = ({
+  component = "button",
+  color = "primary",
+  className,
+  onClick,
+  href = "#",
+  children,
+}) =>
+  React.createElement(
+    component,
+    { className: classNames(styles[`Button_${color}`], className), onClick, href },
+    children
+  );
