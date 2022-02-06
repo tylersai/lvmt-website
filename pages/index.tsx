0a50df1b53d0789129204209eb91ac9a7d5412c6
Sai Han <thesaihan@gmail.com>
2022-02-06 13:06:02 +0630

    Add feature cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,39 @@ interface HomeSectionProps {
 
 const HomeSection: FC<HomeSectionProps> = ({ className, children, sectionTitle, sectionDesc }) => (
   <section className={classNames(className, "py-4 py-md-5")}>
-    <h1 className={classNames(styles.sectionTitle, "d-block text-center text-primary")}>{sectionTitle}</h1>
+    <h3 className={classNames(styles.sectionTitle, "d-block text-center text-primary")}>{sectionTitle}</h3>
     <p className={classNames(styles.sectionDesc, "d-block text-center text-sh-dark")}>{sectionDesc}</p>
     {children}
   </section>
+);
+
+interface FeatureCardProps {
+  className?: string | undefined;
+  titleClassName?: string | undefined;
+  descClassName?: string | undefined;
+  cardImgSrc?: string;
+  cardTitle: string;
+  cardDesc: string;
+}
+
+const FeatureCard: FC<FeatureCardProps> = ({
+  className,
+  titleClassName,
+  descClassName,
+  cardImgSrc,
+  cardTitle,
+  cardDesc,
+}) => (
+  <div
+    className={classNames(
+      styles.featureCard,
+      className,
+      "col-10 col-sm-8 col-md-4 col-lg-3 p-4 mx-3 my-4 d-flex flex-column align-items-center"
+    )}
+  >
+    <h4 className={classNames(titleClassName || "text-sh-dark", "text-center")}>{cardTitle}</h4>
+    <p className={classNames(descClassName || "text-sh-gray", "text-center")}>{cardDesc}</p>
+  </div>
 );
 
 const Home: NextPage = () => {
@@ -40,10 +69,26 @@ const Home: NextPage = () => {
           </Button>
         </div>
       </section>
-      <HomeSection
-        sectionTitle="features"
-        sectionDesc="E360 provides a wide range of features and functions"
-      ></HomeSection>
+      <HomeSection sectionTitle="features" sectionDesc="E360 provides a wide range of features and functions">
+        <div className="row justify-content-evenly align-items-stretch">
+          <FeatureCard
+            cardTitle="Client"
+            titleClassName="text-cyan-500"
+            cardDesc="Create and manage your clients. Record client information either as a person or a company to contact, send
+              out invoices."
+          />
+          <FeatureCard
+            cardTitle="Case"
+            titleClassName="text-indigo-400"
+            cardDesc="Create and manage your client’s legal cases. Key-in your work as time entry and keep track of expenses under each case."
+          />
+          <FeatureCard
+            cardTitle="Lawyer"
+            titleClassName="text-pink-400"
+            cardDesc="Create lawyer profiles with your team and manage all your lawyers in a single place. Assign different roles to each lawyer."
+          />
+        </div>
+      </HomeSection>
       <section className="d-flex flex-column justify-content-center align-items-center">
         <h1 className={styles.title2}>Welcome to E360</h1>
         <p className={styles.description2}>
