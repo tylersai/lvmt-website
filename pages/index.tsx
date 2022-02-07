51b4f387189d8b56b3e01f5ee5cb1b412634a89b
Sai Han <thesaihan@gmail.com>
2022-02-07 16:05:31 +0630

    Add review text

--- pages/index.tsx
+++ pages/index.tsx
@@ -223,6 +223,11 @@ const Home: NextPage<{ steps: Step[] }> = ({ steps }) => {
                 role="button"
               ></i>
             </div>
+            <p className="text-center fw-400 mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat.
+            </p>
           </div>
         </div>
       </HomeSection>
