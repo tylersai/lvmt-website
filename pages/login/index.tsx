cf38e5f18fc9dde3779ef544fd7d98db71805299
Sai Han <thesaihan@gmail.com>
2022-02-20 21:18:55 +0630

    login page layout

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,16 +1,19 @@
 import classNames from "classnames";
 import type { NextPage } from "next";
 import { FC, useCallback, useState } from "react";
-import { Button, PageLayout } from "@components";
+import { Button, CommonHead, PageLayout } from "@components";
 import styles from "@styles/LoginPage.module.scss";
 
 const imgPref = "/login";
 
 const LoginPage: NextPage = () => {
   return (
-    <PageLayout>
+    <PageLayout hideHeader hideFooter CommonHeadComp={<CommonHead title={"E360 \u2022 Login"} />}>
       <section className="row justify-content-center align-items-center">
-        <h2 className="text-center">Login Page</h2>
+        <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
+          <h2 className="text-center text-primary">E360 Login</h2>
+        </div>
+        <div className="col-md-6 align-self-stretch d-none d-md-flex flex-column justify-content-center align-items-center bg-primary"></div>
       </section>
     </PageLayout>
   );
