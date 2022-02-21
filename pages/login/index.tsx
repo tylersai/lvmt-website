d78134cd78e2e5b6d4cb84f7efa929375c514d06
Sai Han <thesaihan@gmail.com>
2022-02-21 13:46:24 +0630

    show login wrapper in login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,20 +1,17 @@
 import classNames from "classnames";
 import type { NextPage } from "next";
 import { FC, useCallback, useState } from "react";
-import { Button, CommonHead, PageLayout } from "@components";
+import { Button, CommonHead, LoginSignupWrapper, PageLayout } from "@components";
 import styles from "@styles/LoginPage.module.scss";
-
-const imgPref = "/login";
 
 const LoginPage: NextPage = () => {
   return (
     <PageLayout hideHeader hideFooter CommonHeadComp={<CommonHead title={"E360 \u2022 Login"} />}>
-      <section className="row justify-content-center align-items-center">
-        <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
-          <h2 className="text-center text-primary">E360 Login</h2>
-        </div>
-        <div className="col-md-6 align-self-stretch d-none d-md-flex flex-column justify-content-center align-items-center bg-primary"></div>
-      </section>
+      <LoginSignupWrapper formType="login" desc="See your profile, purchases and payment info.">
+        <form>
+          <input type="text"></input>
+        </form>
+      </LoginSignupWrapper>
     </PageLayout>
   );
 };
