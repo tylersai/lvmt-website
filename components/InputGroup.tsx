431b7564581bfb9081d14f4966aa0b8130dd44c6
Sai Han <thesaihan@gmail.com>
2022-03-04 15:50:24 +0630

    Accept defaultValue, lableClassName, inputClassName and messageClassName in InputGroup component

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -7,11 +7,15 @@ interface InputGroupProps {
   label?: string | undefined;
   placeholder?: string | undefined;
   inputType?: HTMLInputTypeAttribute | undefined;
+  defaultValue?: string | number | readonly string[] | undefined;
   value?: string | number | readonly string[] | undefined;
   onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
   style?: CSSProperties | undefined;
   required?: boolean | undefined;
   disabled?: boolean | undefined;
+  labelClassName?: string | undefined;
+  inputClassName?: string | undefined;
+  messageClassName?: string | undefined;
 }
 
 export const InputGroup: FC<InputGroupProps> = ({
@@ -21,22 +25,28 @@ export const InputGroup: FC<InputGroupProps> = ({
   placeholder,
   inputType,
   value,
+  defaultValue,
   onChange,
   style,
   required,
   disabled,
+  labelClassName,
+  inputClassName,
+  messageClassName,
 }) => {
   const randStr = useMemo(() => `input_id_${Math.random().toString().substring(2, 6)}`, []);
 
   return (
     <div className={classNames("d-flex flex-column align-items-stretch", styles.InputGroup, className)} style={style}>
       {label && (
-        <label htmlFor={randStr}>
+        <label htmlFor={randStr} className={labelClassName}>
           {label}
           {required && " *"}
         </label>
       )}
       <input
+        className={inputClassName}
+        defaultValue={defaultValue}
         name={name}
         id={randStr}
         type={inputType}
