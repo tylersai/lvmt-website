5321cdfe00fd27962d2de96ba036bb4936a7c129
Sai Han <thesaihan@gmail.com>
2022-03-15 13:51:42 +0630

    Accept error message in InputGroup component

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -1,15 +1,17 @@
 import classNames from "classnames";
-import { ChangeEventHandler, CSSProperties, FC, HTMLInputTypeAttribute, useMemo } from "react";
+import { ChangeEventHandler, CSSProperties, FC, FocusEventHandler, HTMLInputTypeAttribute, useMemo } from "react";
 import styles from "@styles/InputGroup.module.scss";
 interface InputGroupProps {
   name?: string | undefined;
   className?: string | undefined;
   label?: string | undefined;
+  message?: string | undefined;
   placeholder?: string | undefined;
   inputType?: HTMLInputTypeAttribute | undefined;
   defaultValue?: string | number | readonly string[] | undefined;
   value?: string | number | readonly string[] | undefined;
   onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
+  onFocus?: FocusEventHandler<HTMLInputElement> | undefined;
   style?: CSSProperties | undefined;
   required?: boolean | undefined;
   disabled?: boolean | undefined;
@@ -22,11 +24,13 @@ export const InputGroup: FC<InputGroupProps> = ({
   name,
   className,
   label,
+  message,
   placeholder,
   inputType,
   value,
   defaultValue,
   onChange,
+  onFocus,
   style,
   required,
   disabled,
@@ -37,7 +41,15 @@ export const InputGroup: FC<InputGroupProps> = ({
   const randStr = useMemo(() => `input_id_${Math.random().toString().substring(2, 6)}`, []);
 
   return (
-    <div className={classNames("d-flex flex-column align-items-stretch", styles.InputGroup, className)} style={style}>
+    <div
+      className={classNames(
+        "d-flex flex-column align-items-stretch position-relative",
+        styles.InputGroup,
+        className,
+        message && styles.InputGroup_error
+      )}
+      style={style}
+    >
       {label && (
         <label htmlFor={randStr} className={labelClassName}>
           {label}
@@ -52,9 +64,11 @@ export const InputGroup: FC<InputGroupProps> = ({
         type={inputType}
         value={value}
         onChange={onChange}
+        onFocus={onFocus}
         placeholder={placeholder}
         disabled={disabled}
       />
+      {message && <small className={classNames("position-absolute fs-10 fw-400", messageClassName)}>{message}</small>}
     </div>
   );
 };
