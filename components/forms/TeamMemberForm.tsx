1543f016d8c5a6c1e7b8286a31d26f082e634a56
Sai Han <thesaihan@gmail.com>
2022-05-12 22:51:00 +0630

    Show select boxes for staff types and role types (unstyled)

--- components/forms/TeamMemberForm.tsx
+++ components/forms/TeamMemberForm.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 import { InputGroup } from "@components";
 import { CSSProperties, FC, FormEventHandler } from "react";
 import buttonStyles from "@styles/Button.module.scss";
+import { useSelector } from "react-redux";
+import { CommonSelectorType } from "types/redux";
 
 interface TeamMemberFormProps {
   className?: string;
@@ -10,6 +12,16 @@ interface TeamMemberFormProps {
 }
 
 export const TeamMemberForm: FC<TeamMemberFormProps> = ({ onSubmit, className, style }) => {
+  const {
+    loading: roleTypeListLoading,
+    data: roleTypeListData,
+    error: roleTypeListError,
+  } = useSelector<any, CommonSelectorType>((state) => state.roleTypeList);
+  const {
+    loading: staffTypeListLoading,
+    data: staffTypeListData,
+    error: staffTypeListError,
+  } = useSelector<any, CommonSelectorType>((state) => state.staffTypeList);
   return (
     <form className={classNames("TeamMemberForm", className)} style={style} onSubmit={onSubmit}>
       <div className="row g-3">
@@ -30,6 +42,32 @@ export const TeamMemberForm: FC<TeamMemberFormProps> = ({ onSubmit, className, s
         </div>
         <div className="col-6 col-lg-5">
           <InputGroup defaultValue={0.0} min={0} label="Global Hourly Rate" inputType="number" required />
+        </div>
+        <div className="col col-lg-6">
+          <label htmlFor="accessRole">Access Role (Role Type)</label>
+          <select className="form-select form-select-sm" id="accessRole">
+            {!roleTypeListLoading &&
+              !roleTypeListError &&
+              roleTypeListData &&
+              roleTypeListData.map((el) => (
+                <option key={el.id} value={el.id} disabled={el.active !== undefined && !el.active}>
+                  {el.value}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="col col-lg-6">
+          <label htmlFor="position">Position (Staff Type)</label>
+          <select className="form-select form-select-sm" id="position">
+            {!staffTypeListLoading &&
+              !staffTypeListError &&
+              staffTypeListData &&
+              staffTypeListData.map((el) => (
+                <option key={el.id} value={el.id} disabled={el.active !== undefined && !el.active}>
+                  {el.value}
+                </option>
+              ))}
+          </select>
         </div>
         <div className="col col-lg-6">
           <InputGroup defaultValue="" label="Email" inputType="email" required />
