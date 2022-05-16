607f27ec8c4c9f98cb9fbf1bd7db1712341caf44
Sai Han <thesaihan@gmail.com>
2022-05-16 22:09:56 +0630

    TeamMemberForm: value/onChange two ways data binding with redux state

--- components/forms/TeamMemberForm.tsx
+++ components/forms/TeamMemberForm.tsx
@@ -1,10 +1,12 @@
 import classNames from "classnames";
 import { InputGroup, SelectGroup } from "@components";
-import { CSSProperties, FC, FormEventHandler } from "react";
+import { ChangeEventHandler, CSSProperties, FC, FormEventHandler, useMemo } from "react";
 import buttonStyles from "@styles/Button.module.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CommonSelectorType } from "types/redux";
 import { RootState } from "redux/store";
+import { TeamMemberForm as TeamMemberFormType } from "types/model";
+import { TeamMemberFormAT } from "redux/actionTypes";
 
 interface TeamMemberFormProps {
   className?: string;
@@ -13,56 +15,128 @@ interface TeamMemberFormProps {
 }
 
 export const TeamMemberForm: FC<TeamMemberFormProps> = ({ onSubmit = (e) => e.preventDefault(), className, style }) => {
+  const dispatch = useDispatch();
   const { data: roleTypeListData } = useSelector<RootState, CommonSelectorType>((state) => state.roleTypeList);
   const { data: staffTypeListData } = useSelector<RootState, CommonSelectorType>((state) => state.staffTypeList);
   const { data: salutationListData } = useSelector<RootState, CommonSelectorType>((state) => state.salutationList);
 
-  const filteredRoleTypes =
-    roleTypeListData && roleTypeListData.length > 0
-      ? roleTypeListData.filter(
-          (el) => el.value !== "Company Admin" && el.value !== "Admin/Manager" && el.value !== "Portal Admin"
-        )
-      : [];
+  const teamMember = useSelector<RootState, TeamMemberFormType>((state) => state.teamMemberForm);
+
+  const filteredRoleTypes = useMemo(
+    () =>
+      roleTypeListData && roleTypeListData.length > 0
+        ? roleTypeListData.filter(
+            (el) => el.value !== "Company Admin" && el.value !== "Admin/Manager" && el.value !== "Portal Admin"
+          )
+        : [],
+    [roleTypeListData]
+  );
+
+  const changeHandler: ChangeEventHandler<HTMLInputElement | HTMLSelectElement> = (e) =>
+    dispatch({ type: TeamMemberFormAT.CHANGE, payload: { [e.target.name]: e.target.value } });
 
   return (
     <form className={classNames("TeamMemberForm", className)} style={style} onSubmit={onSubmit}>
       <div className="row g-3">
         <div className="col-6 col-lg-2">
-          <SelectGroup name="salutation" label="Salutation" options={salutationListData || []} required />
+          <SelectGroup
+            name="salutation"
+            label="Salutation"
+            options={salutationListData || []}
+            required
+            value={teamMember.salutation}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col col-lg-5">
-          <InputGroup name="firstName" defaultValue="" label="First Name" inputType="text" required />
+          <InputGroup
+            name="firstName"
+            label="First Name"
+            inputType="text"
+            required
+            value={teamMember.firstName}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col col-lg-5">
-          <InputGroup name="lastName" defaultValue="" label="Last Name" inputType="text" required />
+          <InputGroup
+            name="lastName"
+            label="Last Name"
+            inputType="text"
+            required
+            value={teamMember.lastName}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col-6 col-lg-2">
-          <InputGroup name="initials" defaultValue="" label="Initials" inputType="text" required />
+          <InputGroup
+            name="initials"
+            label="Initials"
+            inputType="text"
+            required
+            value={teamMember.initials}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col-6 col-lg-5">
-          <InputGroup name="phone" defaultValue="" label="Phone" inputType="text" />
+          <InputGroup
+            name="phone"
+            label="Phone"
+            inputType="text"
+            value={teamMember.phone || ""}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col-6 col-lg-5">
           <InputGroup
             name="globalHourlyRate"
-            defaultValue={0.0}
             min={0}
             label="Global Hourly Rate"
             inputType="number"
             required
+            value={teamMember.globalHourlyRate}
+            onChange={changeHandler}
           />
         </div>
         <div className="col col-lg-6">
-          <SelectGroup name="roleType" label="Access Role (Role Type)" options={filteredRoleTypes} required />
+          <SelectGroup
+            name="roleType"
+            label="Access Role (Role Type)"
+            options={filteredRoleTypes}
+            required
+            value={teamMember.roleType}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col col-lg-6">
-          <SelectGroup name="staffType" label="Position (Staff Type)" options={staffTypeListData || []} required />
+          <SelectGroup
+            name="staffType"
+            label="Position (Staff Type)"
+            options={staffTypeListData || []}
+            required
+            value={teamMember.staffType}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col col-lg-6">
-          <InputGroup name="email" defaultValue="" label="Email" inputType="email" required />
+          <InputGroup
+            name="email"
+            label="Email"
+            inputType="email"
+            required
+            value={teamMember.email}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col col-lg-6">
-          <InputGroup name="password" defaultValue="" label="Password" inputType="password" required />
+          <InputGroup
+            name="password"
+            label="Password"
+            inputType="password"
+            required
+            value={teamMember.password}
+            onChange={changeHandler}
+          />
         </div>
         <div className="col-12 d-flex justify-content-end">
           <input type="submit" value="Create" className={classNames(buttonStyles.Button_primary, "py-2 mt-1")} />
